1a7fa613c5533af01b288e86a701d985b0db2e7d
iman zulfiqar <imanzulfiqar298@gmail.com>
2023-02-28 17:06:18 +0500

    commiting

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script >
+export default {
+  data() {
+    return {
+      images:['./src/assets/bree.jpg','./src/assets/el.jpg',"./src/assets/klara.jpg","./src/assets/nikita.jpg"]
+    }
+  },
+}
+</script>
+
+<template>
+
+ <div class="container">
+  <div v-for="img in images"><img v-bind:src=img></div>
+ </div>
+</template>
+
+<style scoped>
+.container{   padding: 10px;
+  width: 100vw;
+  min-height: 100vh;  background: #021927;
+  display: flex;  flex-wrap: wrap; gap: 2rem;
+  justify-content: space-evenly; align-items: center; overflow: hidden;
+}
+.container div{
+  height: 350px;
+  width: 200px;  
+}
+div img{
+max-width: 300px;
+width: 100%;
+max-height: 600px; 
+}
+</style>
