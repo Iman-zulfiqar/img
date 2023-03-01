761691c3eed97b733787662157d8e910719bbd3b
iman zulfiqar <imanzulfiqar298@gmail.com>
2023-03-01 12:51:15 +0500

    creatting a child component and passing props from parent componenrt to child componet

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,27 @@
-<script >
-export default {
-  data() {
-    return {
-      images:['./bree.jpg','./el.jpg',"./klara.jpg","./nikita.jpg"]
-    }
-  },
-}
-</script>
-
 <template>
-
- <div class="container">
-  <div v-for="img in images"><img v-bind:src=img></div>
- </div>
+<child :img=images />
 </template>
 
-<style scoped>
-.container{   padding: 10px;
-  width: 100vw;
-  min-height: 100vh;  background: #021927;
-  display: flex;  flex-wrap: wrap; gap: 2rem;
-  justify-content: space-evenly; align-items: center; overflow: hidden;
-}
-.container div{
-  height: 350px;
-  width: 200px;  
-}
-div img{
-max-width: 300px;
-width: 100%;
-max-height: 600px; 
-}
+<script>
+import child from './components/child.vue'
+export default {
+  name:'App',
+    components: {
+        child,
+    },
+    data() {
+        return {
+            images: [
+                "./src/assets/bree.jpg",
+                "./src/assets/el.jpg",
+                "./src/assets/klara.jpg",
+                "./src/assets/nikita.jpg",
+
+            ],
+        };
+    },
+};
+</script>
+
+<style>
 </style>
---
+++ src/components/child.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="container">
+    <div v-for="i in img"><img :src="i"></div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'child',
+    props: {
+        img: Array
+    },
+};
+</script>
+
+<style scoped>
+.container {
+    padding: 10px;
+    width: 100vw;
+    min-height: 100vh;
+    background: #021927;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+    justify-content: space-evenly;
+    align-items: center;
+    overflow: hidden;
+}
+
+.container div {
+    height: 350px;
+    width: 200px;
+}
+
+div img {
+    max-width: 300px;
+    width: 100%;
+    max-height: 600px;
+}
+</style>
